9a90e10a974ae75467f7194a1689f230a9eab4f5
Shoaib <s.fazal16@gmail.com>
2015-03-22 01:04:49 +0500

    SugarCRM installation

---
+++ custom/Extension/application/Ext/Language/es_es.SecurityGroups.php
@@ -0,0 +1,8 @@
+<?php
+
+$app_list_strings["moduleList"]["SecurityGroups"] = 'AdministraciÃ³n de Grupos de Seguridad';
+$app_strings['LBL_LOGIN_AS'] = "Conexión como ";
+$app_strings['LBL_LOGOUT_AS'] = "Cerrar sesión como ";
+$app_strings['LBL_SECURITYGROUP'] = 'AdministraciÃ³n de Grupo';
+
+?>
