9022f17a48be69378da2185a0267b7ca989b9c3f
Shoaib <s.fazal16@gmail.com>
2015-04-27 21:08:27 +0500

    Missing Queries

--- custom/modules/KReports/customActivityReport.php
+++ custom/modules/KReports/customActivityReport.php
@@ -16,4 +16,21 @@
 	$getSellersContactedToday = 'SELECT count(id) FROM aos_contracts WHERE customer_signed_date = CURDATE() AND deleted = 0';
 	
 	$result_getSellersContactedToday = $db->query($getSellersContactedToday);
+	
+	$getTasksCompletedToday = "SELECT count(tasks.id) 
+FROM `tasks`
+WHERE tasks.status = 'Completed' AND tasks.date_modified = CURDATE() AND tasks.deleted = 0 AND tasks.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
+GROUP BY tasks.assigned_user_id";
+	
+	$result_getTasksCompletedToday = $db->query($getTasksCompletedToday);
+	
+	$getTasksNotCompletedToday = "SELECT count(tasks.id) 
+FROM `tasks`
+WHERE tasks.status != 'Completed' AND tasks.date_due = CURDATE() AND tasks.deleted = 0 AND tasks.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
+GROUP BY tasks.assigned_user_id";
+	
+	 $result_getTasksNotCompletedToday = $db->query($getTasksNotCompletedToday);
+	
+	
+	
 ?>
