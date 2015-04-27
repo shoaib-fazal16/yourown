26024d4e5b86efbc449a2aee1cc207b153c6f6c3
mohsin188 <mohsin.mumtaz@rolustech.com>
2015-04-28 02:01:50 +0500

    Activity Report

--- custom/modules/KReports/customActivityReport.php
+++ custom/modules/KReports/customActivityReport.php
@@ -1,36 +1,117 @@
 <?php
+	require_once('include/tcpdf/config/lang/eng.php');
+	require_once('custom/modules/KReports/tcpdf/tcpdf.php');
+	
 	global $db, $current_user;
 	
-	$getAllPropertiesPhaseCount = 'SELECT count(id) as "count", phase FROM aos_products WHERE deleted = 0 GROUP BY phase;';
+	$ss = new Sugar_Smarty();
+	
+	
+	$getAllPropertiesPhaseCount = 'SELECT count(id) as "count", phase FROM aos_products WHERE deleted = 0 GROUP BY phase';
 	
 	$result_getAllPropertiesPhaseCount = $db->query($getAllPropertiesPhaseCount);
+	$marketing_count = 0;
+	$sold_count = 0;
+	$escrow_count = 0;
+	while($row  = $db->fetchByAssoc($result_getAllPropertiesPhaseCount))
+	{
+		if($row['phase'] == 'Marketing')
+			$marketing_count = $row['count'];
+		if($row['phase'] == 'Sold')
+			$sold_count = $row['count'];
+		if($row['phase'] == 'Escrow')
+			$escrow_count = $row['count'];
+	}
+	$ss->assign('MARKETING_COUNT', $marketing_count);
+	$ss->assign('SOLD_COUNT', $sold_count);
+	$ss->assign('ESCROW_COUNT', $escrow_count);
 	
 	$getPropertiesAddedToday = 'SELECT count(id) as "count", phase FROM aos_products WHERE date_entered = CURDATE() AND deleted = 0 GROUP BY phase';
 	
 	$result_getPropertiesAddedToday = $db->query($getPropertiesAddedToday);
+	$marketing_added = 0;
+	$sold_added = 0;
+	$escrow_added = 0;
+	while($row  = $db->fetchByAssoc($result_getPropertiesAddedToday))
+	{
+		if($row['phase'] == 'Marketing')
+			$marketing_added = $row['count'];
+		if($row['phase'] == 'Sold')
+			$sold_added = $row['count'];
+		if($row['phase'] == 'Escrow')
+			$escrow_added = $row['count'];
+	}
+	$ss->assign('MARKETING_ADDED', $marketing_added);
+	$ss->assign('SOLD_ADDED', $sold_added);
+	$ss->assign('ESCROW_ADDED', $escrow_added);
 	
 	$getCountOffersMade = 'SELECT count(id) as "count" FROM aos_contracts WHERE offer_mailed_date = CURDATE() AND deleted = 0';
-	
+	$offer_made = 0;
 	$result_getCountOffersMade = $db->query($getCountOffersMade);
+	$row  = $db->fetchByAssoc($result_getCountOffersMade);
+	$offer_made = $row['count'];
 	
-	$getSellersContactedToday = 'SELECT count(id) FROM aos_contracts WHERE customer_signed_date = CURDATE() AND deleted = 0';
+	$ss->assign('OFFER_MADE', $offer_made);
 	
-	$result_getSellersContactedToday = $db->query($getSellersContactedToday);
+	//$getSellersContactedToday = 'SELECT count(id) FROM aos_contracts WHERE customer_signed_date = CURDATE() AND deleted = 0';
 	
-	$getTasksCompletedToday = "SELECT count(tasks.id) 
-FROM `tasks`
-WHERE tasks.status = 'Completed' AND tasks.date_modified = CURDATE() AND tasks.deleted = 0 AND tasks.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
+	//$result_getSellersContactedToday = $db->query($getSellersContactedToday);
+	
+	$getTasksCompletedToday = "SELECT * 
+FROM tasks
+WHERE tasks.status = 'Completed' AND tasks.deleted = 0 AND tasks.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
 GROUP BY tasks.assigned_user_id";
-	
+	$diedre_tasks = array();
+	$shanna_tasks = array();
 	$result_getTasksCompletedToday = $db->query($getTasksCompletedToday);
+	while($row  = $db->fetchByAssoc($result_getTasksCompletedToday))
+	{
+
+		if($row['assigned_user_id'] == '2cfa6793-fb33-1643-af59-54eb4a2c83d9'){
+			$diedre_tasks[] = $row;
+		}
+		else if($row['assigned_user_id'] == '73290ce7-b771-0068-429b-54e24445b8d0')
+		{
+			$shanna_tasks[] = $row;
+		}
+	}	
+//	echo "<pre>";
+//	print_r($diedre_tasks);
+//	die();
+	$ss->assign('DIEDRE_TASKS', $diedre_tasks);
+	$ss->assign('SHANNA_TASKS', $shanna_tasks);
 	
-	$getTasksNotCompletedToday = "SELECT count(tasks.id) 
+/* 	$getTasksNotCompletedToday = "SELECT count(tasks.id) 
 FROM `tasks`
 WHERE tasks.status != 'Completed' AND tasks.date_due = CURDATE() AND tasks.deleted = 0 AND tasks.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
 GROUP BY tasks.assigned_user_id";
 	
-	 $result_getTasksNotCompletedToday = $db->query($getTasksNotCompletedToday);
-	
-	
-	
+	$result_getTasksNotCompletedToday = $db->query($getTasksNotCompletedToday); */
+	$html = $ss->fetch("custom/modules/KReports/tpls/customActivityReport.tpl");
+	if(get_magic_quotes_gpc())
+	{
+		$html = stripslashes($html);
+	}
+//echo $html;
+	$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+	$pdf->setDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+	$pdf->setMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+	$pdf->setHeaderMargin(PDF_MARGIN_HEADER);
+	$pdf->setFooterMargin(PDF_MARGIN_FOOTER);
+	$pdf->setAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+	$pdf->setFontSubsetting(true);
+	$pdf->setFont('helvetica', '', 10, '', true);
+	$pdf->setDisplayMode('fullpage', 'SinglePage', 'UseNone');
+	$pdf->AddPage('L', 'A4');
+	//$pdf->Cell(0, 0, 'A4 LANDSCAPE', 1, 1, 'C');
+
+	$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196), 'opacity'=>1, 'blend_mode'=>'Normal'));
+
+	$pdf->writeHTML($html, true, false, true, false, '');
+	// ---------------------------------------------------------
+	ob_clean();
+	$pdf->Output('ActivityReport.pdf', 'I');
+	die();
 ?>
---
+++ custom/modules/KReports/tpls/customActivityReport.tpl
@@ -0,0 +1,136 @@
+<center>
+	<h3>Daily Activity Report</h3>
+	<table id='detailpanel_1' cellpadding="4" width="100%" border="1">
+		<tr>
+			<td width='30%' scope="row">
+				<b># of properties in Escrow</b>
+			</td>
+			<td width='30%' scope="row">
+				{$ESCROW_COUNT}
+			</td>
+			<td width='30%'  >
+				<b># of properties on market</b>
+			</td>
+			<td width='30%' scope="row">
+				{$MARKETING_COUNT}
+			</td>
+			<td width='30%' scope="row">
+				<b># of properties sold</b>
+			</td>
+			<td width='30%' scope="row">
+				{$SOLD_COUNT}
+			</td>
+		</tr>
+		<tr>
+			<td width='12.5%' scope="row">
+				<b>New Today</b>
+			</td>
+			<td width='12.5%' scope="row">
+				{$ESCROW_ADDED}
+			</td>
+			<td width='37.5%'  >
+				<b>New Today</b>
+			</td>
+			<td width='12.5%' scope="row">
+				{$MARKETING_ADDED}
+			</td>
+			<td width='12.5%' scope="row">
+				<b>New Today</b>
+			</td>
+			<td width='12.5%' scope="row">
+				{$SOLD_ADDED}
+			</td>
+		</tr>
+		<tr>
+			<td width='12.5%' scope="row">
+				<b># of offers made</b>
+			</td>
+			<td width='12.5%' scope="row">
+				{$OFFER_MADE}
+			</td>
+		</tr>
+	</table>
+	<br/>
+	<br/>
+	<table id='detailpanel_1' width="100%" border="1" cellpadding="4">
+		<tr>
+			<td width="15%" scope="row"><b>Diedre</b></td>
+			<td width="12%" scope="row">&nbsp;&nbsp;</td>
+			<td width="15%" scope="row">&nbsp;&nbsp;</td>
+			<td width="15%" scope="row">&nbsp;&nbsp;</td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b>Tasks Completed</b></td>
+			<td width="12%" scope="row"><b>Priority</b></td>
+			<td width="15%" scope="row"><b>Due Date</b></td>
+			<td width="15%" scope="row"><b>Related To</b></td>
+		</tr>
+			{if !empty($DIEDRE_TASKS)}
+				{foreach from=$DIEDRE_TASKS key=val item=task}
+					<tr width="15%" scope="row"><td>{$task.name}</td><td width="12%" scope="row">{$task.priority}</td><td width="15%" scope="row">{$task.date_due}</td><td width="15%" scope="row">{$task.parent_type}</td></tr>
+				{/foreach}
+			{/if}
+	</table>
+		<br/>
+	<br/>
+	<table id='detailpanel_1' width="100%" border="1" cellpadding="4">
+		<tr>
+			<td width="15%" scope="row"><b>Shanna</b></td>
+			<td width="12%" scope="row">&nbsp;&nbsp;</td>
+			<td width="15%" scope="row">&nbsp;&nbsp;</td>
+			<td width="15%" scope="row">&nbsp;&nbsp;</td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b>Tasks Completed</b></td>
+			<td width="12%" scope="row"><b>Priority</b></td>
+			<td width="15%" scope="row"><b>Due Date</b></td>
+			<td width="15%" scope="row"><b>Related To</b></td>
+		</tr>
+			{if !empty($SHANNA_TASKS)}
+				{foreach from=$SHANNA_TASKS key=val item=task}
+					<tr width="15%" scope="row"><td>{$task.name}</td><td width="12%" scope="row">{$task.priority}</td><td width="15%" scope="row">{$task.date_due}</td><td width="15%" scope="row">{$task.parent_type}</td></tr>
+				{/foreach}
+			{/if}
+	</table>
+	<br/><br/>
+	<table id='detailpanel_1' width="100%" border="1" cellpadding="4">
+		<tr>
+			<td width="15%" scope="row">&nbsp;&nbsp;</td>
+			<td width="15%" scope="row"><b>Diedre</b></td>
+			<td width="15%" scope="row"><b>Shanna</b></td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b># of Sellers Contacted Today</b></td>
+			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row"></td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b>Called</b></td>
+			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row"></td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b>Emailed</b></td>
+			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row"></td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row">&nbsp;</td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b># of Buyers Contacted Today</b></td>
+			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row"></td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b>Called</b></td>
+			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row"></td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b>Emailed</b></td>
+			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row"></td>
+		</tr>
+	</table>
+</center>
