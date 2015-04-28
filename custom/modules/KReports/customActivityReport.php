c1994ec7686d59606b49a77a62d5684a528b5055
mohsin188 <mohsin.mumtaz@rolustech.com>
2015-04-28 20:51:07 +0500

    Activity Report

--- custom/modules/KReports/customActivityReport.php
+++ custom/modules/KReports/customActivityReport.php
@@ -53,10 +53,6 @@
 	
 	$ss->assign('OFFER_MADE', $offer_made);
 	
-	//$getSellersContactedToday = 'SELECT count(id) FROM aos_contracts WHERE customer_signed_date = CURDATE() AND deleted = 0';
-	
-	//$result_getSellersContactedToday = $db->query($getSellersContactedToday);
-	
 	$getTasksCompletedToday = "SELECT * 
 FROM tasks
 WHERE tasks.status = 'Completed' AND tasks.deleted = 0 AND tasks.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
@@ -75,18 +71,139 @@ GROUP BY tasks.assigned_user_id";
 			$shanna_tasks[] = $row;
 		}
 	}	
-//	echo "<pre>";
-//	print_r($diedre_tasks);
-//	die();
+
 	$ss->assign('DIEDRE_TASKS', $diedre_tasks);
 	$ss->assign('SHANNA_TASKS', $shanna_tasks);
 	
-/* 	$getTasksNotCompletedToday = "SELECT count(tasks.id) 
+ 	$getTasksNotCompletedToday = "SELECT count(tasks.id) 
 FROM `tasks`
 WHERE tasks.status != 'Completed' AND tasks.date_due = CURDATE() AND tasks.deleted = 0 AND tasks.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
 GROUP BY tasks.assigned_user_id";
+
+	$diedre_uncompletedtasks = array();
+	$shanna_uncompletedtasks = array();
+	$result_getTasksNotCompletedToday = $db->query($getTasksNotCompletedToday);
+	while($row  = $db->fetchByAssoc($result_getTasksNotCompletedToday))
+	{
+
+		if($row['assigned_user_id'] == '2cfa6793-fb33-1643-af59-54eb4a2c83d9'){
+			$diedre_uncompletedtasks[] = $row;
+		}
+		else if($row['assigned_user_id'] == '73290ce7-b771-0068-429b-54e24445b8d0')
+		{
+			$shanna_uncompletedtasks[] = $row;
+		}
+	}	
+
+	$ss->assign('DIEDRE_UNCOMPLETEDTASKS', $diedre_uncompletedtasks);
+	$ss->assign('SHANNA_UNCOMPLETEDTASKS', $shanna_uncompletedtasks);
+
+	$getSellersContactedTodayviaCall = "SELECT count(leads.id) as count, calls.assigned_user_id
+FROM leads
+LEFT JOIN calls
+ON leads.id = calls.parent_id
+WHERE calls.date_entered = CURDATE() OR calls.date_modified = CURDATE() AND calls.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
+GROUP BY calls.assigned_user_id ";
 	
-	$result_getTasksNotCompletedToday = $db->query($getTasksNotCompletedToday); */
+	$result_getSellersContactedTodayviaCall = $db->query($getSellersContactedTodayviaCall);
+	$diedre_SellersContactedTodayviaCall = 0;
+	$shanna_SellersContactedTodayviaCall = 0;
+	
+	while($row  = $db->fetchByAssoc($result_getSellersContactedTodayviaCall))
+	{
+		if($row['assigned_user_id'] == '2cfa6793-fb33-1643-af59-54eb4a2c83d9'){
+			$diedre_SellersContactedTodayviaCall = $row['count'];
+		}
+		else if($row['assigned_user_id'] == '73290ce7-b771-0068-429b-54e24445b8d0')
+		{
+			$shanna_SellersContactedTodayviaCall = $row['count'];
+		}
+	}
+	
+	$ss->assign('DIEDRESELLERSCALL', $diedre_SellersContactedTodayviaCall);
+	$ss->assign('SHANNASELLERSCALL', $shanna_SellersContactedTodayviaCall);
+	
+	
+	$getBuyersContactedTodayviaCall = "SELECT count(accounts.id) as count, calls.assigned_user_id
+FROM accounts
+LEFT JOIN calls
+ON accounts.id = calls.parent_id
+WHERE calls.date_entered = CURDATE() OR calls.date_modified = CURDATE() AND  calls.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
+GROUP BY calls.assigned_user_id ";
+	
+	$result_getBuyersContactedTodayviaCall = $db->query($getBuyersContactedTodayviaCall);
+	$diedre_BuyersContactedTodayviaCall = 0;
+	$shanna_BuyersContactedTodayviaCall = 0;
+	
+	while($row  = $db->fetchByAssoc($result_getBuyersContactedTodayviaCall))
+	{
+		if($row['assigned_user_id'] == '2cfa6793-fb33-1643-af59-54eb4a2c83d9'){
+			$diedre_BuyersContactedTodayviaCall = $row['count'];
+		}
+		else if($row['assigned_user_id'] == '73290ce7-b771-0068-429b-54e24445b8d0')
+		{
+			$shanna_BuyersContactedTodayviaCall = $row['count'];
+		}
+	}
+	
+	$ss->assign('DIEDREBUYERSCALL', $diedre_BuyersContactedTodayviaCall);
+	$ss->assign('SHANNABUYERSCALL', $shanna_BuyersContactedTodayviaCall);
+	
+	$getBuyersContactedTodayviaEmail = "SELECT count(accounts.id) as count, emails.assigned_user_id
+FROM accounts
+LEFT JOIN emails
+ON accounts.id = emails.parent_id
+WHERE emails.date_sent = CURDATE() AND emails.status='sent' AND emails.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
+GROUP BY emails.assigned_user_id ";
+	
+	$result_getBuyersContactedTodayviaEmail = $db->query($getBuyersContactedTodayviaEmail);
+	$diedre_BuyersContactedTodayviaEmail = 0;
+	$shanna_BuyersContactedTodayviaEmail = 0;
+	
+	while($row  = $db->fetchByAssoc($result_getBuyersContactedTodayviaEmail))
+	{
+		if($row['assigned_user_id'] == '2cfa6793-fb33-1643-af59-54eb4a2c83d9'){
+			$diedre_BuyersContactedTodayviaEmail = $row['count'];
+		}
+		else if($row['assigned_user_id'] == '73290ce7-b771-0068-429b-54e24445b8d0')
+		{
+			$shanna_BuyersContactedTodayviaEmail = $row['count'];
+		}
+	}
+	
+	$ss->assign('DIEDREBUYERSEMAIL', $diedre_BuyersContactedTodayviaEmail);
+	$ss->assign('SHANNABUYERSEMAIL', $shanna_BuyersContactedTodayviaEmail);
+	
+	
+	$getSellersContactedTodayviaEmail = "SELECT count(leads.id) as count, emails.assigned_user_id
+FROM leads
+LEFT JOIN emails
+ON leads.id = emails.parent_id
+WHERE emails.date_sent = CURDATE() AND emails.status='sent' AND emails.assigned_user_id IN ('2cfa6793-fb33-1643-af59-54eb4a2c83d9','73290ce7-b771-0068-429b-54e24445b8d0')
+GROUP BY emails.assigned_user_id ";
+	
+	$result_getSellersContactedTodayviaEmail = $db->query($getSellersContactedTodayviaEmail);
+	$diedre_SellersContactedTodayviaEmail = 0;
+	$shanna_SellersContactedTodayviaEmail = 0;
+	
+	while($row  = $db->fetchByAssoc($result_getSellersContactedTodayviaEmail))
+	{
+		if($row['assigned_user_id'] == '2cfa6793-fb33-1643-af59-54eb4a2c83d9'){
+			$diedre_SellersContactedTodayviaEmail = $row['count'];
+		}
+		else if($row['assigned_user_id'] == '73290ce7-b771-0068-429b-54e24445b8d0')
+		{
+			$shanna_SellersContactedTodayviaEmail = $row['count'];
+		}
+	}
+	
+	$ss->assign('DIEDRESELLERSEMAIL', $diedre_SellersContactedTodayviaEmail);
+	$ss->assign('SHANNASELLERSEMAIL', $shanna_SellersContactedTodayviaEmail);
+	$ss->assign('SHANNASELLERSCONTACTED', $shanna_SellersContactedTodayviaEmail+$shanna_SellersContactedTodayviaCall);
+	$ss->assign('SHANNABUYERSCONTACTED', $shanna_BuyersContactedTodayviaEmail+$shanna_BuyersContactedTodayviaCall);
+	$ss->assign('DIEDRESELLERSCONTACTED', $diedre_SellersContactedTodayviaEmail+$diedre_SellersContactedTodayviaCall);
+	$ss->assign('DIEDREBUYERSCONTACTED', $diedre_BuyersContactedTodayviaEmail+$diedre_BuyersContactedTodayviaCall);
+	
 	$html = $ss->fetch("custom/modules/KReports/tpls/customActivityReport.tpl");
 	if(get_magic_quotes_gpc())
 	{
--- custom/modules/KReports/tpls/customActivityReport.tpl
+++ custom/modules/KReports/tpls/customActivityReport.tpl
@@ -95,42 +95,82 @@
 	<br/><br/>
 	<table id='detailpanel_1' width="100%" border="1" cellpadding="4">
 		<tr>
+			<td width="15%" scope="row"><b>Diedre</b></td>
+			<td width="12%" scope="row">&nbsp;&nbsp;</td>
+			<td width="15%" scope="row">&nbsp;&nbsp;</td>
+			<td width="15%" scope="row">&nbsp;&nbsp;</td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b>Tasks Not Completed</b></td>
+			<td width="12%" scope="row"><b>Priority</b></td>
+			<td width="15%" scope="row"><b>Due Date</b></td>
+			<td width="15%" scope="row"><b>Related To</b></td>
+		</tr>
+			{if !empty($DIEDRE_UNCOMPLETEDTASKS)}
+				{foreach from=$DIEDRE_UNCOMPLETEDTASKS key=val item=task}
+					<tr width="15%" scope="row"><td>{$task.name}</td><td width="12%" scope="row">{$task.priority}</td><td width="15%" scope="row">{$task.date_due}</td><td width="15%" scope="row">{$task.parent_type}</td></tr>
+				{/foreach}
+			{/if}
+	</table>
+	<br/><br/>
+	<table id='detailpanel_1' width="100%" border="1" cellpadding="4">
+		<tr>
+			<td width="15%" scope="row"><b>Shanna</b></td>
+			<td width="12%" scope="row">&nbsp;&nbsp;</td>
+			<td width="15%" scope="row">&nbsp;&nbsp;</td>
+			<td width="15%" scope="row">&nbsp;&nbsp;</td>
+		</tr>
+		<tr>
+			<td width="15%" scope="row"><b>Tasks Not Completed</b></td>
+			<td width="12%" scope="row"><b>Priority</b></td>
+			<td width="15%" scope="row"><b>Due Date</b></td>
+			<td width="15%" scope="row"><b>Related To</b></td>
+		</tr>
+			{if !empty($SHANNA_UNCOMPLETEDTASKS)}
+				{foreach from=$SHANNA_UNCOMPLETEDTASKS key=val item=task}
+					<tr width="15%" scope="row"><td>{$task.name}</td><td width="12%" scope="row">{$task.priority}</td><td width="15%" scope="row">{$task.date_due}</td><td width="15%" scope="row">{$task.parent_type}</td></tr>
+				{/foreach}
+			{/if}
+	</table>
+	<br/><br/>
+	<table id='detailpanel_1' width="100%" border="1" cellpadding="4">
+		<tr>
 			<td width="15%" scope="row">&nbsp;&nbsp;</td>
 			<td width="15%" scope="row"><b>Diedre</b></td>
 			<td width="15%" scope="row"><b>Shanna</b></td>
 		</tr>
 		<tr>
 			<td width="15%" scope="row"><b># of Sellers Contacted Today</b></td>
-			<td width="15%" scope="row"></td>
-			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row">{$DIEDRESELLERSCONTACTED}</td>
+			<td width="15%" scope="row">{$SHANNASELLERSCONTACTED}</td>
 		</tr>
 		<tr>
 			<td width="15%" scope="row"><b>Called</b></td>
-			<td width="15%" scope="row"></td>
-			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row">{$DIEDRESELLERSCALL}</td>
+			<td width="15%" scope="row">{$SHANNASELLERSCALL}</td>
 		</tr>
 		<tr>
 			<td width="15%" scope="row"><b>Emailed</b></td>
-			<td width="15%" scope="row"></td>
-			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row">{$DIEDRESELLERSEMAIL}</td>
+			<td width="15%" scope="row">{$SHANNASELLERSEMAIL}</td>
 		</tr>
 		<tr>
 			<td width="15%" scope="row">&nbsp;</td>
 		</tr>
 		<tr>
 			<td width="15%" scope="row"><b># of Buyers Contacted Today</b></td>
-			<td width="15%" scope="row"></td>
-			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row">{$DIEDREBUYERSCONTACTED}</td>
+			<td width="15%" scope="row">{$SHANNABUYERSCONTACTED}</td>
 		</tr>
 		<tr>
 			<td width="15%" scope="row"><b>Called</b></td>
-			<td width="15%" scope="row"></td>
-			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row">{$DIEDREBUYERSCALL}</td>
+			<td width="15%" scope="row">{$SHANNABUYERSCALL}</td>
 		</tr>
 		<tr>
 			<td width="15%" scope="row"><b>Emailed</b></td>
-			<td width="15%" scope="row"></td>
-			<td width="15%" scope="row"></td>
+			<td width="15%" scope="row">{$DIEDREBUYERSEMAIL}</td>
+			<td width="15%" scope="row">{$SHANNABUYERSEMAIL}</td>
 		</tr>
 	</table>
 </center>
